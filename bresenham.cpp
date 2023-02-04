925930c6395530ded34e5ff4cf6b6be6ce3eb20e
Utkarsh Rastogi <95937956+URRG@users.noreply.github.com>
2023-02-04 13:27:09 +0530

    Update bresenham.cpp

--- bresenham.cpp
+++ bresenham.cpp
@@ -75,12 +75,12 @@ void main(int argc, char **argv) {
 	printf( "Enter (x1, y1, x2, y2)\n");
 	scanf("%d %d %d %d", &x1, &y1, &x2, &y2);
 
-	glutInit(&argc, argv);
-	glutInitDisplayMode(GLUT_SINGLE|GLUT_RGB);
-	glutInitWindowSize(500, 500);
-	glutInitWindowPosition(0, 0);
-	glutCreateWindow("Bresenham's Line Drawing");
+	glfwInit(&argc, argv);
+	glfwInitDisplayMode(GLUT_SINGLE|GLUT_RGB);
+	glfwInitWindowSize(500, 500);
+	glfwInitWindowPosition(0, 0);
+	glfwCreateWindow("Bresenham's Line Drawing");
 	myInit();
-	glutDisplayFunc(myDisplay);
-	glutMainLoop();
+	glfwDisplayFunc(myDisplay);
+	glfwMainLoop();
 }
